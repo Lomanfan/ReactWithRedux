4e97c2a55fdda19eb2cb1f778389c1fd1232a533
Lomanfan <lfan.canada@gmail.com>
2022-03-14 20:36:41 -0600

    add componentDidMount

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,14 +5,12 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
