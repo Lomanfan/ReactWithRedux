08759dda515a5eaa81d37cec27be8846fa97ae7f
Lomanfan <lfan.canada@gmail.com>
2022-03-23 01:10:35 -0600

    add SearchBar

---
+++ we-tube-videos/src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    //TODO: Call callback from parent component
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
