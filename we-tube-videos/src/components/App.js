52c420398f5e902abf80786ce6c0522986254750
Lomanfan <lfan.canada@gmail.com>
2022-03-24 23:21:20 -0600

    add VideoDetail

--- we-tube-videos/src/components/App.js
+++ we-tube-videos/src/components/App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: { q: term },
     });
 
-    console.log(response);
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
