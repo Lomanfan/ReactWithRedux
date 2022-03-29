7de16c5f51db14180fa3946e6751b4b2c9ce591c
Lomanfan <lfan.canada@gmail.com>
2022-03-28 22:32:41 -0600

    display videos

--- we-tube-videos/src/components/App.js
+++ we-tube-videos/src/components/App.js
@@ -7,12 +7,19 @@ import VideoDetail from "./VideoDetail";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: { q: term },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -23,11 +30,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- we-tube-videos/src/components/VideoItem.js
+++ we-tube-videos/src/components/VideoItem.js
@@ -4,7 +4,11 @@ import "./VideoItem.css";
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div className="video-item item" onClick={() => onVideoSelect(video)}>
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
