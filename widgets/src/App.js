94a139f5c33d1dd3a1c1489244ec3fc068d3f373
Lomanfan <lfan.canada@gmail.com>
2022-03-30 23:24:19 -0600

    add Accordion component

---
+++ widgets/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript library/framework.",
+  },
+  { title: "Why use React?", content: "React can create reusable components." },
+  { title: "How do you use React?", content: "npx create-react-app your-app" },
+];
+
+export default () => {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
