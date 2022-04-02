380f0e003a3cffe6ea0fe5dc4b9a0ba49d0e858a
Lomanfan <lfan.canada@gmail.com>
2022-04-01 23:47:13 -0600

    add Search component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
@@ -16,7 +17,7 @@ const items = [
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
