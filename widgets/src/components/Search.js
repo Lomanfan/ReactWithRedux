d9bff9913f89940ce342939e427f1dfab45f90b5
Lomanfan <lfan.canada@gmail.com>
2022-04-06 19:38:19 -0600

    update Search component

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    console.log('HI')
+  }, [])
+
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term</label>
-          <input className="input" />
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
         </div>
       </div>
     </div>
